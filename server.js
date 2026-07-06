489cc8cbc56f53f5adcfa1c20277f41b47a18170
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-06 19:55:19 +0500

    add api test client with google oauth bearer support

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/teamRoster
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.use(passport.initialize());
 
 mongoose.connect(MONGO_URI)
