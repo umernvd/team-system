f2594a4302f4b0b8c842c2e100435c3d846640fc
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-06 18:27:11 +0500

    add api test client for testing auth and employee crud

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
+// Serve the API test client
+app.use(express.static('public'));
+
 connectDB();
 
 // Public auth routes
