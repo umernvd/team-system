cfb3d3a23733600fc3d87a7e4c2a0bbc5e9bc2f9
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-07 15:50:19 +0500

    add express.static('public') to serve test client

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.use(express.static('public'));
 app.use(requestLogger);
 connectDB();
 app.use('/employees', employeeRoutes);
