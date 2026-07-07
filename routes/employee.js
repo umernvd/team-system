a21afed83cec15cbd70562a14032d3364ac08c3b
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-07 13:32:28 +0500

    refactor employee module with validation middleware

--- routes/employee.js
+++ routes/employee.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const basicAuth = require('../middleware/basicAuth');
-const roleCheck = require('../middleware/roleCheck');
-
-const {
-    getAllEmployees,
-    createEmployee,
-    updateEmployee,
-    deleteEmployee
-} = require('../controllers/employeeController');
-
 const router = express.Router();
+const basicAuth = require('../middlewares/basicAuth');
+const roleCheck = require('../middlewares/roleCheck');
+const validate = require('../middlewares/validate');
+const { validateCreate, validateUpdate, validateDelete } = require('../validators/employeeValidator');
+const employeeController = require('../controllers/employeeController');
 
-router.get('/', basicAuth, getAllEmployees);
-router.post('/', basicAuth, roleCheck('admin'), createEmployee);
-router.put('/:id', basicAuth, roleCheck('admin'), updateEmployee);
-router.delete('/:id', basicAuth, roleCheck('admin'), deleteEmployee);
+router.get('/', basicAuth, employeeController.getAll);
+router.post('/', basicAuth, roleCheck('admin'), validate(validateCreate), employeeController.create);
+router.put('/:id', basicAuth, roleCheck('admin'), validate(validateUpdate), employeeController.update);
+router.delete('/:id', basicAuth, roleCheck('admin'), validate(validateDelete), employeeController.delete);
 
 module.exports = router;
