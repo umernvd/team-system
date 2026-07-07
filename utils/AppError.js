623da1e0447cd23ea5faec1dd1107bfabf0f6205
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-07 14:43:46 +0500

    add env config error utilities and response helpers

---
+++ utils/AppError.js
@@ -0,0 +1,8 @@
+class AppError extends Error {
+    constructor(message, statusCode, errors) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors || [];
+    }
+}
+module.exports = AppError;
