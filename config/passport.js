553e5c69f929dda7d60e954c1b289fd4f55381bd
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-07 15:59:11 +0500

    fix passport config to use process.env instead of removed config.js

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } = require('../config');
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL;
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
