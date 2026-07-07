1da6b80dd039bc2e8bed5ea1e07a66b474d0fde6
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-07 15:45:43 +0500

    update server.js with new middleware chain, remove dead helpers and data files

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const connectDB = require('./db');
-const Employee = require('./models/Employee');
 const employeeRoutes = require('./routes/employee');
-
+const webhookRoutes = require('./routes/webhook');
+const requestLogger = require('./middlewares/requestLogger');
+const notFound = require('./middlewares/notFound');
+const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Middleware to parse JSON bodies
 app.use(express.json());
-
+app.use(requestLogger);
 connectDB();
-
 app.use('/employees', employeeRoutes);
-
+app.use('/webhooks', webhookRoutes);
+app.use(notFound);
+app.use(errorHandler);
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
