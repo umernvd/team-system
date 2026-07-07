f9ea105e80ae47a1e3239723e23f59a26b41fc7f
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-07 13:30:26 +0500

    add webhook event model and database repositories

---
+++ repositories/employeeRepository.js
@@ -0,0 +1,26 @@
+const Employee = require('../models/Employee');
+
+exports.findAll = async () => {
+  return Employee.find({});
+};
+
+exports.findById = async (id) => {
+  return Employee.findById(id);
+};
+
+exports.create = async (data) => {
+  const employee = new Employee(data);
+  return employee.save();
+};
+
+exports.bulkCreate = async (data) => {
+  return Employee.insertMany(data, { ordered: false });
+};
+
+exports.update = async (id, data) => {
+  return Employee.findByIdAndUpdate(id, data, { new: true });
+};
+
+exports.delete = async (id) => {
+  return Employee.findByIdAndDelete(id);
+};
