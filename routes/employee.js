c5a954b1f1693dae9dcde26554e03eb0b089990a
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-20 16:14:28 +0500

    update routes with granular permission checks

--- routes/employee.js
+++ routes/employee.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../middlewares/authenticateToken');
-const roleCheck = require('../middlewares/roleCheck');
+const checkPermission = require('../middlewares/checkPermission');
 const validate = require('../middlewares/validate');
 const { validateCreate, validateUpdate, validateDelete } = require('../validators/employeeValidator');
 const employeeController = require('../controllers/employeeController');
 
-router.get('/', authenticateToken, employeeController.getAll);
-router.post('/', authenticateToken, roleCheck('admin'), validate(validateCreate), employeeController.create);
-router.put('/:id', authenticateToken, roleCheck('admin'), validate(validateUpdate), employeeController.update);
-router.delete('/:id', authenticateToken, roleCheck('admin'), validate(validateDelete), employeeController.delete);
+router.get('/',     authenticateToken, checkPermission('employees:read'),   employeeController.getAll);
+router.post('/',    authenticateToken, checkPermission('employees:create'), validate(validateCreate), employeeController.create);
+router.put('/:id',  authenticateToken, checkPermission('employees:update'), validate(validateUpdate), employeeController.update);
+router.delete('/:id', authenticateToken, checkPermission('employees:delete'), validate(validateDelete), employeeController.delete);
 
 module.exports = router;
