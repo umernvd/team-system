c0bab03ae4127e19c89a3e80e6f731f7908aac42
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-20 16:13:28 +0500

    remove auth routes controllers services and validators

--- validators/authTokenValidator.js
+++
@@ -1,9 +0,0 @@
-function validateRefreshToken(body) {
-  const errors = [];
-  if (!body || !body.refreshToken || typeof body.refreshToken !== 'string' || !body.refreshToken.trim()) {
-    errors.push('Refresh token is required');
-  }
-  return errors;
-}
-
-module.exports = { validateRefreshToken };
